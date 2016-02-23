4c9fc2bbdc66404ad78a6b0fbd50de5aa505aa2c
PE <https://payamE@bitbucket.org>
2016-02-23 12:13:48 +0100

    Uploading the readme files

--- CreateDockersForOpenMSTools.sh
+++ CreateDockersForOpenMSTools.sh
@@ -1,7 +1,7 @@
 
 OPENMS_CMAKE_FILE="`wget -qO- 'https://raw.githubusercontent.com/OpenMS/OpenMS/master/src/topp/executables.cmake'
 `"
-#echo $OPENMS_CMAKE_FILE
+
 
 IFS=$'\n' read -rd '' -a split_CMAK <<< "$OPENMS_CMAKE_FILE"
 control=0
@@ -32,11 +32,14 @@ for i in "${split_CMAK[@]}"; do
 			echo "Setting the Entry Point to \"$i\""
 			entryPoint=$i
 			echo "Preparing docker information"
-			DockerData="FROM ubuntu:14.04\nMAINTAINER Payam Emami, payam.emami@medsci.uu.se\nRUN apt-get update && apt-get install --yes openms\nENTRYPOINT [\"$entryPoint\"]"
+			#DockerData="FROM ubuntu:14.04\nMAINTAINER Payam Emami, payam.emami@medsci.uu.se\nRUN apt-get update && apt-get install --yes openms\nENTRYPOINT [\"$entryPoint\"]"
 			echo "writing Dokcer file"
 			echo -e  $DockerData > $i/Dockerfile
-			(cd $i && exec docker build -t ${i,,} .)
+			#(cd $i && exec docker build -t ${i,,} .)
 			echo "A docker has been created for \"$i\""
+			echo "Creating \"readme\" File"
+			readmeData="#A Docker container for \"$i\" tool in OpenMS\nFor more information about the tool please visit:\nhttp://ftp.mi.fu-berlin.de/pub/OpenMS/documentation/html/TOPP_$i.html"
+			echo -e  $readmeData > $i/README.md
 			echo "************"
         fi
 	if [[ $i == "set(TOPP_executables" ]]
